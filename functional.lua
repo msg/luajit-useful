a9630009dc588fd566a4102c5e49d1bd5dfcb654
Matt Gerassimoff <mgeras@gmail.com>
2020-08-31 05:01:53 -0700

    rename to functional.

--- functional.lua
+++ functional.lua
@@ -1,17 +1,17 @@
 --
--- u s e f u l / f u n c t i o n s . l u a
+-- u s e f u l / f u n c t i o n a l . l u a
 --
-local functions = { }
+local functional = { }
 
 local system		= require('useful.system')
 local  loadstring	=  system.loadstring
 
-function functions.callable(obj)
+function functional.callable(obj)
 	return type(obj) == 'function' or getmetatable(obj) and
 			getmetatable(obj).__call
 end
 
-function functions.memoize(func)
+function functional.memoize(func)
 	return setmetatable({ }, {
 		__index = function(self, key, ...)
 			local v = func(key, ...)
@@ -22,42 +22,43 @@ function functions.memoize(func)
 	})
 end
 
-function functions.lambda(func)
-	local code = 'return function(a,b,c,d,e,f,g) return ' .. func .. ' end'
+function functional.lambda(code)
+	code = 'return function(a,b,c,d,e,f,g) return ' .. code .. ' end'
 	local chunk = assert(loadstring(code, 'tmp'))
 	return chunk()
 end
 
-local lambdas = functions.memoize()
+local lambdas = functional.memoize()
 
-function functions.function_arg(func)
+local function_arg = function(func)
 	if type(func) == 'string' then
 		func = lambdas(func)
 	else
-		assert(functions.callable(func),
+		assert(functional.callable(func),
 			'expecting function or callable object')
 	end
 	return func
 end
+functional.function_arg = function_arg
 
-function functions.bind1(func, a)
-	func = functions.function_arg(func)
+function functional.bind1(func, a)
+	func = function_arg(func)
 	return function(...)
 		return func(a, ...)
 	end
 end
 
-function functions.bind2(func, b)
-	func = functions.function_arg(func)
+function functional.bind2(func, b)
+	func = function_arg(func)
 	return function(a, ...)
 		return func(a, b, ...)
 	end
 end
 
-function functions.compose(...)
+function functional.compose(...)
 	local funcs = { }
 	for _,func in pairs({...}) do
-		table.insert(funcs, 1, functions.function_arg(func))
+		table.insert(funcs, 1, function_arg(func))
 	end
 	return function(...)
 		local v = funcs[1](...)
@@ -68,12 +69,12 @@ function functions.compose(...)
 	end
 end
 
-function functions.take2(func)
-	func = functions.function_arg(func)
+function functional.take2(func)
+	func = function_arg(func)
 	return function(...)
 		local _,value = func(...)
 		return value
 	end
 end
 
-return functions
+return functional
