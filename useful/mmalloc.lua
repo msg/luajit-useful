1352e037d0573513f9154c8b30301634b9fafafd
Matt Gerassimoff <mgeras@gmail.com>
2021-06-16 19:04:42 -0700

    add no_gc option.

--- useful/mmalloc.lua
+++ useful/mmalloc.lua
@@ -18,7 +18,12 @@ function mmalloc.align(sz)
 	return band(sz + mask - 1, bnot(mask))
 end
 
-function mmalloc.mmalloc(sz)
+function mmalloc.mfree(p, sz)
+	local msz = mmalloc.align(sz)
+	C.munmap(p, msz)
+end
+
+function mmalloc.mmalloc(sz, no_gc)
 	local prot	= bor(C.PROT_READ, C.PROT_WRITE)
 	local map	= bor(C.MAP_PRIVATE, C.MAP_ANONYMOUS)
 	local msz	= mmalloc.align(sz)
@@ -26,9 +31,11 @@ function mmalloc.mmalloc(sz)
 	if p == sys_mman.MAP_FAILED then
 		return ffi.cast('void *', nil)
 	else
-		return ffi.gc(p, function()
-			C.munmap(p, msz)
-		end)
+		if no_gc == true then
+			return p
+		else
+			return ffi.gc(p, function() mmalloc.mfree(p, sz) end)
+		end
 	end
 end
 
