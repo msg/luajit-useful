26072165b08a3aa51854f07ea61a6fb118cb83fe
Matt Gerassimoff <mgeras@gmail.com>
2022-05-09 15:04:20 -0700

    remove packet stuff because it really should be in rpc.

--- useful/range/msgpack.lua
+++ useful/range/msgpack.lua
@@ -426,45 +426,4 @@ msgpack.decode = function (data)
 	return decode(data)
 end
 
-local PACKET_HEADER_SIZE	= 12
-msgpack.PACKET_HEADER_SIZE	= PACKET_HEADER_SIZE
-
--- NOTE: these functions modify the incoming range (r8) which is useful
---       to append more messages before sending the output.
-
-
-local encode_header = function(length, r8)
-	local v16	= uint16.vla(1)
-	v16[0]		= uint16.swap(length)
-	encode('MSGPACK', r8)
-	encode(v16, r8)
-end
-msgpack.encode_header = encode_header
-
-msgpack.encode_packet = function(data, r8)
-	local sr8	= r8:save()
-	r8:pop_front(PACKET_HEADER_SIZE)	-- make room for header
-	local p8	= r8:save()
-	encode(data, r8)
-	p8.back		= r8.front
-
-	encode_header(#p8, sr8:save())
-	return uint8.meta(sr8.front, r8.front)
-end
-
-local decode_header = function(r8)
-	assert(r8:get_front()	== FIXSTR + 7,	'sync bad')
-	assert(decode(r8)	== 'MSGPACK',	'sync MSGPACK not found')
-	assert(r8:get_front()	== BIN8,	'length BIN8 not found')
-	local length = decode(r8)
-	return tonumber(uint16.swap(uint16.to_vla(length)[0]))
-end
-msgpack.decode_header = decode_header
-
-msgpack.decode_packet = function(r8)
-	local length = decode_header(r8)
-	assert(length <= #r8, 'invalid length')
-	return decode(r8)
-end
-
 return msgpack
