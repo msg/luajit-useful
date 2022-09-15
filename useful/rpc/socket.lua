89f8edd2fec664f9c67e3643802ff6cbcad37d87
Matt Gerassimoff <mgeras@gmail.com>
2022-09-15 12:45:49 -0700

    allocate vla and range in ctor. cleanups

--- useful/rpc/socket.lua
+++ useful/rpc/socket.lua
@@ -3,7 +3,7 @@
 -- u s e f u l / r p c / s o c k e t . l u a
 --
 
-local rpc_socket = { }
+local socket = { }
 
 local class		= require('useful.class')
 local  Class		=  class.Class
@@ -13,33 +13,33 @@ local msgpack		= require('useful.range.msgpack')
 local  decode		=  msgpack.decode
 local rpc		= require('useful.rpc')
 local  RPC		=  rpc.RPC
-local rpc_message	= require('useful.rpc.message')
-local  decode_header	=  rpc_message.decode_header
-local  encode_message	=  rpc_message.encode_message
-local  HEADER_SIZE	=  rpc_message.HEADER_SIZE
+local message		= require('useful.rpc.message')
+local  decode_header	=  message.decode_header
+local  encode_message	=  message.encode_message
+local  HEADER_SIZE	=  message.HEADER_SIZE
 
-rpc_socket.TCP_RPC = Class(RPC, {
+socket.TCP_RPC = Class(RPC, {
 	new = function(self, sock, size, timeout)
 		RPC.new(self, timeout)
-		self.size	= size
-		self.sock	= sock
+		self.size		= size
+		self.sock		= sock
+		self.vla, self.u8	= uint8.vla(sick)
 	end,
 
 	send = function(self, msg, to)			--luacheck:ignore
-		local _,o8	= uint8.vla(self.size)
+		local o8	= self.u8:save()
 		local m8	= encode_message(msg, o8)
 		local rc	= self.sock:send_all(m8.front, #m8)
 		return rc
 	end,
 
 	recv = function(self, timeout)
-		local _,in8	= uint8.vla(self.size)
-		local i8	= in8:save()
+		local i8	= self.u8:save()
 		self.sock:set_timeout(timeout)
 		i8.back = i8.front + self.sock:recv_all(i8.front, HEADER_SIZE)
 		local length	= decode_header(i8)
 		assert(length <= #in8)
-		i8		= in8:save()
+		i8		= self.u8:save()
 		i8.back = i8.front + self.sock:recv_all(i8.front, length)
 		return decode(i8), nil
 	end,
@@ -47,4 +47,4 @@ rpc_socket.TCP_RPC = Class(RPC, {
 	__index = RPC.__index,
 })
 
-return rpc_socket
+return socket
