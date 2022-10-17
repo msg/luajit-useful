0a209ef88358b4bfa7f4bc5cd8d6a3df942b48bc
Matt Gerassimoff <mgeras@gmail.com>
2022-10-17 06:12:04 -0700

    fix syntax errors.

--- useful/rpc/socket.lua
+++ useful/rpc/socket.lua
@@ -23,7 +23,7 @@ socket.TCP_RPC = Class(RPC, {
 		RPC.new(self, timeout)
 		self.size		= size
 		self.sock		= sock
-		self.vla, self.u8	= uint8.vla(sick)
+		self.vla, self.u8	= uint8.vla(size)
 	end,
 
 	send = function(self, msg, to)			--luacheck:ignore
@@ -38,7 +38,7 @@ socket.TCP_RPC = Class(RPC, {
 		self.sock:set_timeout(timeout)
 		i8.back = i8.front + self.sock:recv_all(i8.front, HEADER_SIZE)
 		local length	= decode_header(i8)
-		assert(length <= #in8)
+		assert(length <= #i8)
 		i8		= self.u8:save()
 		i8.back = i8.front + self.sock:recv_all(i8.front, length)
 		return decode(i8), nil
