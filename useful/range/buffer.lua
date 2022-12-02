c9a3515a8b023c1e815da89800cb7de4904a63e3
Matt Gerassimoff <mgeras@gmail.com>
2022-12-02 10:07:45 -0700

    better error handling. cleanups. fix writef() to use lua sprintf().

--- useful/range/buffer.lua
+++ useful/range/buffer.lua
@@ -1,16 +1,17 @@
 --
 -- b u f f e r . l u a
 --
-local  min	=  math.min
+local  min		=  math.min
 
-local ffi	= require('ffi')
-local  C	=  ffi.C
-local  copy	=  ffi.copy
+local ffi		= require('ffi')
+local  copy		=  ffi.copy
 
-local class	= require('useful.class')
-local  Class 	=  class.Class
-local range	= require('useful.range')
-local  int8	=  range.int8
+local class		= require('useful.class')
+local  Class 		=  class.Class
+local range		= require('useful.range')
+local  char		=  range.char
+local stdio		= require('useful.stdio')
+local  sprintf		=  stdio.sprintf
 
 local buffer = { }
 
@@ -27,8 +28,8 @@ buffer.find_nl = find_nl
 
 buffer.Buffer = Class({
 	new = function(self, size, read_func, write_func)
-		self.buffer, self.free = int8.vla(size)
-		self.avail	= int8(self.free.front, self.free.front)
+		self.buffer, self.free = char.vla(size)
+		self.avail	= char(self.free.front, self.free.front)
 		self.read_func	= read_func
 		self.write_func	= write_func
 	end,
@@ -43,51 +44,65 @@ buffer.Buffer = Class({
 	end,
 
 	flush = function(self, force)
-		assert(force or #self.avail == 0, 'available bytes in buffer')
+		if not force and #self.avail ~= 0 then
+			error('#avail is not zero. #avail='..tostring(#self.avail))
+		end
 		self.free.front		= self.buffer
 		self.avail.front	= self.buffer
 		self.avail.back		= self.avail.front
+		return 0
 	end,
 
 	flush_write = function(self)
 		local nbytes = 0
 		local avail = self.avail
-		while #avail > 0 do
-			local n = self.write_func(avail.front, #avail)
-			if n >= 0 then
-				nbytes = nbytes + n
-				avail:pop_front(n)
-			elseif nbytes > 0 then
-				return nbytes
-			else
-				return n
+		local r, err
+		while #avail > 0 do			--luacheck:ignore
+			r, err = self.write_func(avail.front, #avail)
+			if not r then
+				break
 			end
+			nbytes = nbytes + r
+			avail:pop_front(r)
 		end
-		self:flush()
-		return nbytes
+		if nbytes > 0 and #avail > 0 then	-- wrote some
+			return nbytes
+		elseif not r then			-- wrote none
+			return r, err
+		end
+		r, err = self:flush()			-- wrote all
+		if not r then
+			return r, err, nbytes
+		else
+			return nbytes
+		end
 	end,
 
 	read_more = function(self, nbytes)
-		nbytes = min(nbytes, #self.free)
-		local n = self.read_func(self.free.front, nbytes)
-		if n > 0 then
-			self:pop_insert(n)
+		nbytes = nbytes or #self.free
+		local n, err = self.read_func(self.free.front, nbytes)
+		if not n then
+			return n, err
+		elseif n == 0 then
+			return nil, 'timeout'
 		end
+		self:pop_insert(n)
 		return tonumber(n)
 	end,
 
-	read_avail = function(self, n)
-		n = n or 1
-		assert(n <= #self.avail)
-		return self.avail:read_front_size(n or 1)
+	read_avail = function(self, nbytes)
+		nbytes = nbytes or #self.avail
+		local r8 = self.avail:front_range(nbytes)
+		self.avail:pop_front(#r8)
+		return r8
 	end,
 
 	read = function(self, nbytes)
 		nbytes = min(nbytes, #self.avail + #self.free)
 		while #self.avail < nbytes do
-			local rc = self:read_more(nbytes - #self.avail)
-			if rc < 0 then
-				return nil, rc
+			local ok, err = self:read_more(nbytes - #self.avail)
+			if not ok then
+				return ok, err
 			end
 		end
 		local r8 = self.avail:front_range(nbytes)
@@ -104,9 +119,9 @@ buffer.Buffer = Class({
 			elseif #self.free == 0 then
 				return self.avail.read_front_size(#self.avail)
 			end
-			local rc = self:read_more(#self.free)
-			if rc < 0 then
-				return nil, rc
+			local ok, err = self:read_more()
+			if not ok then
+				return ok, err
 			end
 		end
 		line.back	= line.front + 1
@@ -117,7 +132,7 @@ buffer.Buffer = Class({
 
 	write = function(self, data)
 		if type(data) == 'string' then
-			data = self.free.from_string(data)
+			data	= self.free.from_string(data)
 		end
 		local nbytes	= min(#data, #self.free)
 		local r8	= self.free:front_range(nbytes)
@@ -127,11 +142,13 @@ buffer.Buffer = Class({
 	end,
 
 	writef = function(self, ...)
-		local nbytes = C.snprintf(nil, 0, ...)
-		assert(nbytes + 1 <= #self.free, 'not enough space to writef')
-		local r8	= self.free:front_range(nbytes)
-		C.snprintf(r8.front, nbytes+1, ...)
-		self:pop_insert(nbytes)
+		local s = sprintf(...)
+		if #self.free < #s+1 then
+			error('not enough space')
+		end
+		local r8	= self.free:front_range(#s)
+		ffi.copy(r8.front, s, #s)
+		self:pop_insert(#s)
 		return r8
 	end,
 })
