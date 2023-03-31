8d57daaa58a6b3ad39aa9cb80657e9389cff2c69
Matt Gerassimoff <mgeras@gmail.com>
2023-03-30 19:28:20 -0700

    add interfaces() function and use it in config().

--- useful/network.lua
+++ useful/network.lua
@@ -15,61 +15,79 @@ local bits	= require('useful.bits')
 local  getbits	=  bits.getbits
 local json	= require('useful.json')
 
-network.iptos = function(ip)
+local iptos = function(ip)
 	local octets = { }
 	for i=3,0,-1 do
 		table.insert(octets, sprintf('%d', getbits(ip, i*8, 8)))
 	end
 	return table.concat(octets, '.')
 end
+network.iptos = iptos
 
-network.stoip = function(s)
+local stoip = function(s)
 	local ip = 0
 	for octet in s:gmatch('%d+') do
 		ip = bor(lshift(ip, 8), tonumber(octet))
 	end
 	return ip
 end
+network.stoip = stoip
 
-network.mactos = function(mac)
+local mactos = function(mac)
 	local octets = { }
 	for i=5,0,-1 do
 		table.insert(octets, sprintf('%02x', getbits(mac, i*8, 8)))
 	end
 	return table.concat(octets, ':')
 end
+network.mactos = mactos
 
-network.stomac = function(s)
+local stomac = function(s)
 	local mac = 0LL
 	for octet in s:gmatch('[0-9A-Fa-f]+') do
 		mac = bor(lshift(mac, 8), tonumber(octet, 16))
 	end
 	return mac
 end
+network.stomac = stomac
 
 network.gateway = function(ifname)
 	local routes	= json.decode(io.popen('ip -j route'):read('*a'))
 	for _,route in ipairs(routes) do
 		if ifname ~= nil and route.dev ~= ifname then --luacheck:ignore
 		elseif route.gateway ~= nil then
-			return network.stoip(route.gateway)
+			return stoip(route.gateway)
 		end
 	end
 end
 
-network.config = function(ifname)
+local interfaces = function()
+	local interfaces = { }
 	local address	= json.decode(io.popen('ip -j address show'):read('*a'))
 	for _,entry in ipairs(address) do
-		if entry.ifname ~= ifname then		--luacheck:ignore
-		elseif entry.addr_info == nil then	--luacheck:ignore
-		elseif entry.addr_info[1] == nil then	--luacheck:ignore
-		else
-			local info	= entry.addr_info[1]
+		local info	= entry.addr_info[1]
+		if info then
 			local addr	= info['local']
-			local ip	= network.stoip(addr)
 			local nm	= bnot(lshift(1,32-info.prefixlen)-1)
-			local mac	= network.stomac(entry.address)
-			return ip, nm, mac
+			local mac	= stomac(entry.address)
+			local interface = {
+				ifname	= entry.ifname,
+				ip	= addr,
+				nm	= iptos(nm),
+				mac	= mactos(mac),
+			}
+			table.insert(interfaces, interface)
+		end
+	end
+	return interfaces
+end
+network.interfaces = interfaces
+
+network.config = function(ifname)
+	-- TODO: handle interfaces with multiple addresses
+	for _,interface in ipairs(interfaces()) do
+		if interface.ifname == ifname then
+			return interface.ip, interface.nm, interface.mac
 		end
 	end
 end
