276377745cc6286622a94fade2ba45b27e20330c
Matt Gerassimoff <mgeras@gmail.com>
2023-05-25 21:20:05 -0700

    add differing byte-size versions.

--- useful/bits.lua
+++ useful/bits.lua
@@ -24,6 +24,29 @@ function bits.setbits(x, p, n, y)
 	return bor(band(x, bnot(lshift(m, p))), lshift(band(m, y), p))
 end
 
+ffi.cdef [[
+typedef union bits_swap_t {
+	uint16_t u16[4];
+	uint32_t u32[2];
+	uint64_t u64[1];
+} bits_swap_t;
+]]
+local bits_swap = ffi.new('bits_swap_t')
+
+function bits.swap16(value)
+	bits_swap.u16[3] = value
+	return bswap(bits_swap.u64[0])
+end
+
+function bits.swap32(value)
+	bits_swap.u32[1] = value
+	return bswap(bits_swap.u64[0])
+end
+
+function bits.swap64(value)
+	return bswap(value)
+end
+
 function bits.swap(value)
 	local size = sizeof(value)
 	value = cast('int64_t', value)
