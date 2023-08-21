9b093b77aef54805d1a523c733f10bf890a3138e
Matt Gerassimoff <mgeras@gmail.com>
2023-08-21 05:38:15 -0700

    cleaner swap implementations.

--- useful/bits.lua
+++ useful/bits.lua
@@ -3,54 +3,39 @@
 --
 local bits = { }
 
-local ffi	= require('ffi')
-local  cast	=  ffi.cast
-local  sizeof	=  ffi.sizeof
+local ffi		= require('ffi')
+local  C		=  ffi.C
+local  cast		=  ffi.cast
+local  sizeof		=  ffi.sizeof
 
-local bit	= require('bit')
-local  band	=  bit.band
-local  bnot	=  bit.bnot
-local  bor	=  bit.bor
-local  bswap	=  bit.bswap
-local  lshift	=  bit.lshift
-local  rshift	=  bit.rshift
+local bit		= require('bit')
+local  band		=  bit.band
+local  bnot		=  bit.bnot
+local  bor		=  bit.bor
+local  bswap		=  bit.bswap
+local  lshift		=  bit.lshift
+local  rshift		=  bit.rshift
+local  tobit		=  bit.tobit
 
 function bits.getbits(x, p, n)
-	return band(rshift(x, p), bnot(lshift(bnot(0LL), n)))
+	return band(rshift(x, p), bnot(lshift(bnot(0ULL), n)))
 end
 
 function bits.setbits(x, p, n, y)
-	local m = bnot(lshift(bnot(0LL), n))
+	local m = bnot(lshift(bnot(0ULL), n))
 	return bor(band(x, bnot(lshift(m, p))), lshift(band(m, y), p))
 end
 
-ffi.cdef [[
-typedef union bits_swap_t {
-	uint16_t u16[4];
-	uint32_t u32[2];
-	uint64_t u64[1];
-} bits_swap_t;
-]]
-local bits_swap = ffi.new('bits_swap_t')
-
 function bits.swap16(value)
-	bits_swap.u16[3] = value
-	return bswap(bits_swap.u64[0])
+	return rshift(bswap(tobit(value)), 16)
 end
 
 function bits.swap32(value)
-	bits_swap.u32[1] = value
-	return bswap(bits_swap.u64[0])
+	return bswap(tobit(value))
 end
 
 function bits.swap64(value)
 	return bswap(value)
-end
-
-function bits.swap(value)
-	local size = sizeof(value)
-	value = cast('int64_t', value)
-	return rshift(bswap(value), 64 - size * 8)
 end
 
 return bits
