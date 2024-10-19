5e31cffba5627c657c04ba4ae7409adbe0b7cb70
Matt Gerassimoff <mgeras@gmail.com>
2024-10-19 17:28:53 -0500

    align base to page and use offset for real mapping.

--- useful/mmap.lua
+++ useful/mmap.lua
@@ -9,6 +9,8 @@ local  cast		=  ffi.cast
 local  gc		=  ffi.gc
 
 local bit		= require('bit')
+local  band		=  bit.band
+local  bnot		=  bit.bnot
 local  bor		=  bit.bor
 
 		  require('posix.fcntl')
@@ -18,52 +20,61 @@ local  bor		=  bit.bor
 local class	= require('useful.class')
 local  Class	=  class.Class
 
+local function align_to_page(addr)
+	return band(addr, bnot(0xfff))
+end
+
 mmap.MMAP = Class({
 	new = function(self, path, addr, size, options)
 		options = options or {
 			mode = 'read',
 		}
-		self.path = path
-		self.addr = cast('off_t', addr)
-		self.size = cast('size_t', size)
+		self.path	= path
+		self.addr	= cast('off_t', align_to_page(addr))
+		local offset	= addr - self.addr
+		self.size	= cast('size_t', size + offset)
 
-		local prot  = C.PROT_READ
-		local flags = bor(C.O_RDONLY, C.O_SYNC)
-		if     options.mode == 'write' then
+		local prot	= C.PROT_READ
+		local flags	= bor(C.O_RDONLY, C.O_SYNC)
+		if options.mode == 'write' then
 			prot	= C.PROT_WRITE
-			flags	= bor(C.O_WRONLY, C.O_SYNC)
+			flags	= bor(C.O_WRONLY, C.OSYNC)
 		elseif options.mode == 'read_write' then
 			prot	= bor(C.PROT_READ, C.PROT_WRITE)
-			flags	= bor(C.O_RDWR, C.O_SYNC)
+			flags	= bor(C.O_RDWR, C.OSYNC)
 		end
 		self.fd = C.open(path, flags)
 		if self.fd < 0 then
 			self.p = nil
 			return
 		end
-
-		self.p = C.mmap(nil, self.size, prot,
-				C.MAP_SHARED, self.fd, addr)
-		if self.p == C.MAP_FAILED then
+		self.base	= C.mmap(nil, self.size, prot,
+					C.MAP_SHARED, self.fd, self.addr)
+		if self.base == C.MAP_FAILED then
 			C.close(self.fd)
-			self.p = nil
-			self.fd = -1
+			self.base	= nil
+			self.p		= nil
+			self.fd		= -1
 		else
-			self.p = cast('char *', self.p)
+			self.base	= cast('char *', self.base)
+			self.p		= self.base + offset
 			-- cleanup code
-			self.p = gc(self.p, function() self:__gc() end)
+			self.base	= gc(self.base, function()
+				self:__gc()
+			end)
 		end
 	end,
 
 	close = function(self)
-		if self.p ~= nil then
-			C.munmap(self.p, self.size)
+		self.p		= nil
+		if self.base ~= nil then
+			C.munmap(self.base, self.size)
 		end
-		self.p = nil
+		self.base	= nil
 		if self.fd > -1 then
 			C.close(self.fd)
 		end
-		self.fd = -1
+		self.fd		= -1
 	end,
 
 	__gc = function(self) -- tables don't call this metamethod
