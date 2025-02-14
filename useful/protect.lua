fff2554b0ec751d2dd7b9ed2bb8f8e1737a5e904
Matt Gerassimoff <mgeras@gmail.com>
2025-02-14 08:09:59 -0600

    add function descriptions.

--- useful/protect.lua
+++ useful/protect.lua
@@ -22,6 +22,7 @@ protect.traceback_error	= function(message, level)
 end
 protect.error		= protect.default_error
 
+-- throw : throw pcall like return as error
 local throw = function(ok, err, ...)
 	if not ok then
 		protect.error(err, 2) -- non-localized so it can be changed
@@ -31,15 +32,17 @@ local throw = function(ok, err, ...)
 end
 protect.throw = throw
 
+-- throw1: throw pcall like return but use first parameter on good return
 local throw1 = function(v, err, ...)
 	if not v then
 		protect.error(err, 2) -- non-localized so it can be changed
 	else
-		return v, err, ...
+		return v, err, ... -- NOTE: difference from throw above
 	end
 end
 protect.throw1 = throw1
 
+-- unprotect : unprotect call using throw semantics
 protect.unprotect = function(func)
 	assert(type(func) == 'function')
 	return function(...)
@@ -47,6 +50,7 @@ protect.unprotect = function(func)
 	end
 end
 
+-- unprotect : unprotect call using throw1 semantics
 protect.unprotect1 = function(func)
 	assert(type(func) == 'function')
 	return function(...)
@@ -54,6 +58,7 @@ protect.unprotect1 = function(func)
 	end
 end
 
+-- protect : protect call and return values from function on lua stack
 protect.protect = function(func)
 	assert(type(func) == 'function')
 	return function(...)
@@ -66,12 +71,13 @@ protect.protect = function(func)
 	end
 end
 
+-- protect1 : protect call and return values from call in table
 protect.protect1 = function(func)
 	assert(type(func) == 'function')
 	return function(...)
 		local ok, args = pack_ok(pcall(func, ...))
 		if ok then
-			return args
+			return args -- NOTE: difference from protect above
 		else
 			return nil, args[1]
 		end
@@ -81,6 +87,7 @@ end
 local default_errno_string = errno_string_
 protect.errno_string = default_errno_string
 
+-- protect_s : protect C library call with int like return
 protect.protect_s = function(func, errno_string)
 	assert(type(func) == 'function')
 	errno_string = errno_string or default_errno_string
